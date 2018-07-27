f17fb8cad4fbd45c5a11a0720bef176c58724477
Uemit <info@artan-design.de>
2018-07-28 00:03:26 +0200

    first commit

---
+++ recipeError/Helper/ApiHelper.swift
@@ -0,0 +1,9 @@
+//
+//  ApiHelper.swift
+//  recipeError
+//
+//  Created by Ümit Sevim on 27.07.18.
+//  Copyright © 2018 Ümit Sevim. All rights reserved.
+//
+
+import Foundation
---
+++ recipeError/Helper/CoreHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CoreHelper.swift
+//  recipeError
+//
+//  Created by Ümit Sevim on 27.07.18.
+//  Copyright © 2018 Ümit Sevim. All rights reserved.
+//
+
+import Foundation
